1a2e7f8c642448a1d4741b8834b023ef4591a733
Laaifat <yazid.laaifat@gmail.com>
2024-12-09 22:15:38 +0100

    Init

---
+++ src/main/java/mine/app/workoutandhealth/WorkoutAndHealthApplication.java
@@ -0,0 +1,13 @@
+package mine.app.workoutandhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutAndHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutAndHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mine/app/workoutandhealth/WorkoutAndHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package mine.app.workoutandhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAndHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
